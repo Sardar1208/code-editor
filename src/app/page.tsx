a4009aee88a5a352198e550ebbfdd38768667130
Sarthak Bakre <sarthak.bakre@gmail.com>
2023-06-02 01:16:08 +0530

    feat: initialised code editor with text transformation and pointer mangement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,100 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import Prism from "prismjs";
+import { useState, useRef } from "react";
 
 export default function Home() {
+  const [cursorPosition, setCursorPosition] = useState(0);
+  const editorRef = useRef<any>(null);
+  const [code, setCode] = useState("hello");
+
+  function updateCode() {
+    var editor = document.getElementById("main_input");
+    const text = editor ? editor.innerText : "";
+    // console.log("editor: ", editor?.innerText);
+    // setCode(editor!.innerText);
+
+    const currentPosition =
+      window.getSelection()?.getRangeAt(0).startOffset || null;
+    if (currentPosition != null) {
+      setCursorPosition(currentPosition);
+
+      // console.log("cursor position: ", cursorPosition);
+
+      var html = Prism.highlight(
+        text,
+        Prism.languages.javascript,
+        "javascript"
+      );
+
+      // Inject the new highlighted code
+      if (editor != null) {
+        editor.innerHTML = html;
+      }
+
+      console.log("child nodes: ", editorRef.current.childNodes);
+
+      var childNodes = editorRef.current.childNodes;
+      var count = 0;
+      var totalCount = 0;
+      var currentNode = null;
+
+      for (var node of childNodes) {
+        if (node.nodeType == 3) {
+          totalCount += node.length;
+          if(count + node.length < currentPosition){
+            count += node.length;
+          }
+        } else if (node.nodeType == 1) {
+          if(count + node.firstChild.length < currentPosition){
+            count += node.firstChild.length;
+          }
+          totalCount += node.firstChild.length;
+        }
+
+        if (totalCount >= currentPosition) {
+          currentNode = node;
+        }
+      }
+
+      // Restore the cursor position
+      const selection = window.getSelection();
+      const range = document.createRange();
+      try {
+        range.setStart(node, currentPosition - count);
+      }catch(e) {
+        console.log("error is : ", e);
+        range.setStartAfter(currentNode);
+      }
+      range.collapse(true);
+      selection!.removeAllRanges();
+      selection!.addRange(range);
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+    <main className="h-full w-full">
+      <button onClick={() => console.log("changes made", code)}>get log</button>
+      <pre>
+        <code
+          ref={editorRef}
+          id="main_input"
+          contentEditable="true"
+          aria-multiline="true"
+          content={code}
+          onInput={(e) => {
+            // console.log("changes made", e.target);
+            updateCode();
+          }}
+          className="h-full w-full bg-grey-500"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          {code}
+        </code>
+      </pre>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {/* <div contentEditable="true">
+        <span style={{ color: "yellow" }}>const</span> name = new;
+      </div> */}
     </main>
-  )
+  );
 }
