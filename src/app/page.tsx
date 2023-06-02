50a7d9008d96dc92ce41b0e235c2f087d8cb4ffe
Sarthak Bakre <sarthak.bakre@gmail.com>
2023-06-02 22:39:51 +0530

    fix: updated logic of calculating initial cursor offset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,42 @@ import Prism from "prismjs";
 import { useState, useRef } from "react";
 
 export default function Home() {
-  const [cursorPosition, setCursorPosition] = useState(0);
   const editorRef = useRef<any>(null);
   const [code, setCode] = useState("hello");
 
   function updateCode() {
     var editor = document.getElementById("main_input");
     const text = editor ? editor.innerText : "";
-    // console.log("editor: ", editor?.innerText);
-    // setCode(editor!.innerText);
 
-    const currentPosition =
-      window.getSelection()?.getRangeAt(0).startOffset || null;
+    // Get the cursor position before doing any changes
+    var currentPosition;
+    var selection = window.getSelection();
+
+    // loops through all the nodes backwards starting from the current node that cursor is on
+    // to the first node and calculate the total offset of the cursor pointer
+    if (selection!.rangeCount > 0) {
+      var range = selection!.getRangeAt(0);
+      var startOffset = range.startOffset;
+      var totalOffset = startOffset;
+
+      var mainNode: any = range.startContainer;
+      mainNode = mainNode.previousSibling;
+      while (mainNode != null) {
+        if (mainNode.nodeType === Node.TEXT_NODE) {
+          totalOffset += mainNode.nodeValue!.length;
+        }else {
+          totalOffset += mainNode.firstChild!.nodeValue!.length;
+        }
+        mainNode = mainNode.previousSibling;
+      }
+
+      console.log("Total offset:", totalOffset);
+      currentPosition = totalOffset;
+    }
+
+
+    // Generate the highlighted code using Prism
     if (currentPosition != null) {
-      setCursorPosition(currentPosition);
-
-      // console.log("cursor position: ", cursorPosition);
-
       var html = Prism.highlight(
         text,
         Prism.languages.javascript,
@@ -32,21 +51,23 @@ export default function Home() {
         editor.innerHTML = html;
       }
 
-      console.log("child nodes: ", editorRef.current.childNodes);
-
       var childNodes = editorRef.current.childNodes;
-      var count = 0;
-      var totalCount = 0;
+      var count = 0; // Maintaines the the offset for the new cursor position
+      var totalCount = 0; // total count of the index while looping thorough the nodes
       var currentNode = null;
 
+      // Loop through all child nodes and find the current node that the cursor was on.
+      // find the offset that tell exactly which position the cursor was on in the current node
       for (var node of childNodes) {
+        // nodeType = 3 means its a text node
         if (node.nodeType == 3) {
           totalCount += node.length;
-          if(count + node.length < currentPosition){
+          if (count + node.length < currentPosition) {
             count += node.length;
           }
+          // nodeType = 1 means its a span node
         } else if (node.nodeType == 1) {
-          if(count + node.firstChild.length < currentPosition){
+          if (count + node.firstChild.length < currentPosition) {
             count += node.firstChild.length;
           }
           totalCount += node.firstChild.length;
@@ -60,10 +81,15 @@ export default function Home() {
       // Restore the cursor position
       const selection = window.getSelection();
       const range = document.createRange();
+      // TODO - optimize this
+      // TODO - its still breaking if we type "hello sarthak const abcd "
       try {
+        // set the cursor position in case of text node
+        // its causes an index error in case of span node
         range.setStart(node, currentPosition - count);
-      }catch(e) {
+      } catch (e) {
         console.log("error is : ", e);
+        // set cursor position in case of span node
         range.setStartAfter(currentNode);
       }
       range.collapse(true);
